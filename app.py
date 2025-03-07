4e60bb52adc06d8a9bfeec2f702b6c3bbccb609b
nrebel <rebel.nico@googlemail.com>
2025-03-08 00:26:28 +0100

    Update app.py
    revert last commit

--- app.py
+++ app.py
@@ -134,10 +134,7 @@ st.write("Markiere Straßen auf der Karte und berechne die optimale Route.")
 map_ = create_map()
 map_data = st_folium(map_, width=700, height=500, key="map")
 
-cities = sorted(list(german_cities))
-#selected_cities = st.multiselect("Wähle eine oder mehrere Städte:", list(german_cities), default="Ludwigshafen")
-selected_cities = st.selectbox('Stadt auswählen', cities, index=54) # make LU the default city ;)
-
+selected_cities = st.multiselect("Wähle eine oder mehrere Städte:", list(german_cities), default="Ludwigshafen")
 # Button zum Starten der Berechnung
 if st.button("Route berechnen"):
     #G = get_street_graph()
